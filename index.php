20e0ad2b9246abdad0e202783810eb755f68de26
moustafa <alsayedmoustafa101997@gmail.com>
2025-12-27 19:01:08 +0200

    index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Moustafa';
+$age =17;
+$height =10.5;
+$student = "true";
+$array =['android','python'];
+echo "\"My name is [$name], I am [$age] years old, my height is [$height] 'meters', it is [$student] that I am a student,
+and my favorite \programming languages\ are [$array[0]] and [$array[1]].\"";
+?>
